00e266a312be139bd8c3b34e152fcc0055adc9de
Robert Hoekstra <robert@hoekstra.nu>
2018-06-11 01:31:29 +0200

    Altered texts

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,12 +1,17 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
-    'continuous deployment', 'continuous improvement', 'devops')
-adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
-adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
-    'seriously')
-verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
+#buzz = ('continuous testing', 'continuous integration',
+#    'continuous deployment', 'continuous improvement', 'devops')
+buzz = ('schoenlepel', 'vaag', 'computers', 'fietssleutel', 'variatie')
+#adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
+adjectives = ('gedaan', 'glimmend', 'voltooid' 'geintegreerd', 'volledig')
+#adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
+#    'seriously')
+adverbs = ('bijzonder', 'gigantisch', 'enorm', 'extreem',
+    'grappig')
+#verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
+verbs = ('harder', 'beter', 'mooier', 'herbouwd', 'sneller')
 
 def sample(l, n = 1):
     result = random.sample(l, n)
