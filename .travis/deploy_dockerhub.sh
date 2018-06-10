15f2bc096121b301182b9aaf5dcf038016e0e9e6
Robert Hoekstra <robert@hoekstra.nu>
2018-06-11 01:31:58 +0200

    Added exit codes to deploy_dockerhub.sh

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS || exit 1
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG . || exit 1
+docker push $TRAVIS_REPO_SLUG || exit 1
