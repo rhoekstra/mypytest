d64ce99ee45ffe1b3e564c481d3aaf003bb2200d
Robert Hoekstra <robert@hoekstra.nu>
2018-06-11 01:21:51 +0200

    Added error handling in deploy_heroku.sh

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install heroku-container-registry
-docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
-heroku container:release web
+heroku plugins:install heroku-container-registry || exit 1
+docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com || exit 1
+heroku container:push web --app $HEROKU_APP_NAME || exit 1
+heroku container:release web || exit 1
