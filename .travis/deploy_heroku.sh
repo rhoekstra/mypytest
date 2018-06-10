a9d76bb7a2e750339db52ff3d52ff9550a4fc40a
Robert Hoekstra <robert@hoekstra.nu>
2018-06-11 01:24:54 +0200

    Added another fix to heroku call

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -3,4 +3,4 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry || exit 1
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com || exit 1
 heroku container:push web --app $HEROKU_APP_NAME || exit 1
-heroku container:release web || exit 1
+heroku container:release web --app $HEROKU_APP_NAME || exit 1
