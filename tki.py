a51ef53cd4fd114628176218931dc5f00faa56fc
Robert Hoekstra <robert@hoekstra.nu>
2019-05-02 15:37:39 +0200

    Added image and updated tky.py

--- tki.py
+++ tki.py
@@ -1,17 +1,17 @@
 #!/usr/local/bin/python3
 """This is our tutorial on tkinter."""
-from tkinter import Frame, Menu, BOTH, Tk, Label
+import tkinter
 from PIL import Image, ImageTk
 import os
 my_path = os.path.abspath(os.path.dirname(__file__))
 
 
-class Window(Frame):
+class Window(tkinter.Frame):
     """This is the main window class."""
 
     def __init__(self, master=None):
         """Do something useful."""
-        Frame.__init__(self, master)
+        tkinter.Frame.__init__(self, master)
 
         self.master = master
         self.init_window()
@@ -20,33 +20,40 @@ class Window(Frame):
         """Initialize window."""
         self.master.title("GUI")
 
-        self.pack(fill=BOTH, expand=1)
+        self.pack(fill=tkinter.BOTH, expand=1)
         # quitButton = Button(self, text="Quit", command=self.client_exit)
         # quitButton.place(x=0,y=0)
+        self.load_menus()
 
-        menu = Menu(self.master)
+    def load_menus(self):
+        """Load the menus here."""
+        menu = tkinter.Menu(self.master)
         self.master.config(menu=menu)
-        file = Menu(menu)
+        file = tkinter.Menu(menu)
         file.add_command(label='Save', command=self.showTxt)
         file.add_command(label='Exit', command=self.client_exit)
         menu.add_cascade(label='File', menu=file)
 
-        edit = Menu(menu)
+        edit = tkinter.Menu(menu)
         edit.add_command(label='Show Image', command=self.showImg)
         edit.add_command(label='Show Text', command=self.showTxt)
         menu.add_cascade(label='Edit', menu=edit)
 
     def showImg(self):
-        """Show some image."""
-        load = Image.open(os.path.join(my_path, "images", "googlelogo.png"))
-        render = ImageTk.PhotoImage(load)
-        img = Label(self, image=render)
-        img.image = render
-        img.place(x=0, y=0)
+        """Show an image."""
+        load = Image.open(os.path.join(my_path, "images", "img.jpg"))
+        photo = ImageTk.PhotoImage(load)
+
+        # labels can be text or images
+        label = tkinter.Label(self, image=photo)
+        label.image = photo
+        # img.place(x=0, y=0)
+        label.place()
 
     def showTxt(self):
         """Show some Text."""
-        text = Label(self, text="My current working dir is "+os.getcwd())
+        text = tkinter.Label(self, text="My current working dir is " +
+                             os.getcwd())
         text.pack()
 
     def client_exit(self):
@@ -58,7 +65,7 @@ class Window(Frame):
         pass
 
 
-root = Tk()
+root = tkinter.Tk()
 
 root.geometry("800x600")
 
